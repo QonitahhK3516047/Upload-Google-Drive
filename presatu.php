a9ee68e31ae53e0672bdddfedfdb7263147bd72e
QonitahhK3516047 <50458827+QonitahhK3516047@users.noreply.github.com>
2019-05-10 08:31:58 +0700

    Add files via upload

---
+++ presatu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="predua.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="selected_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
